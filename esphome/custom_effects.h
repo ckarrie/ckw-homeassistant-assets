a83ae64dd04d04b0f101b20a3642fc405a88efd8
Christian Karrié <ckarrie@users.noreply.github.com>
2023-09-09 17:07:23 +0200

    Update custom_effects.h

--- esphome/custom_effects.h
+++ esphome/custom_effects.h
@@ -1,3 +1,29 @@
+/*
+
+Usage:
+
+esphome:
+  includes:
+    - /config/ckw-homeassistant-assets/esphome/custom_effects.h
+
+light:
+  - platform: neopixelbus
+    type: GRB
+    variant: WS2811
+    pin: GPIO5
+    num_leds: 12
+    name: "NeoPixel Light D1"
+    effects: 
+      - addressable_lambda:
+          name: "github update_power_leds"
+          update_interval: 10ms
+          lambda: |-
+            update_power_leds(it, current_color, initial_run, 50.0);
+
+*/
+
+
+
 void update_power_leds(AddressableLight &it, Color &selected_color, bool initial_run, float watts){
   static uint16_t num_leds = it.size();
   static uint16_t progress = 0;

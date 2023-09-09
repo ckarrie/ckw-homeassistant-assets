0021a2a66c9c08e4a82bc9739a4984609e59b272
Christian Karrié <ckarrie@users.noreply.github.com>
2023-09-09 16:59:23 +0200

    Update custom_effects.h

--- esphome/custom_effects.h
+++ esphome/custom_effects.h
@@ -1,7 +1,7 @@
 void update_power_leds(AddressableLight &it, Color &selected_color, bool initial_run, float watts){
   static uint16_t num_leds = it.size();
   static uint16_t progress = 0;
-  static uint16_t reset_progress = 100;
+  static uint16_t reset_progress = 10;
   if (initial_run){
     it.all() = Color::BLACK;
     progress = 0;
@@ -10,5 +10,7 @@ void update_power_leds(AddressableLight &it, Color &selected_color, bool initial
   if (progress > reset_progress){
     it.all() = selected_color;
     progress = 0;
+  } else {
+    it.all() = Color::BLACK;
   }
 }
